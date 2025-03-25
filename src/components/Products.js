ba4db6a96ba40868c3f8ae424ddf5f53581b2ba2
kamran-icat <kamran.haider@icat.ac.in>
2025-03-25 18:00:23 +0530

    aadede css files

--- src/components/Products.js
+++ src/components/Products.js
@@ -7,9 +7,15 @@ function Products() {
         <div className='first-section'>
             <img className='side-img' src="https://framerusercontent.com/images/CkZeSiNQSOCeZlmD2gatB7yUI.png?scale-down-to=512" />
             <div className='text-container'>
-                <h1>Transforming data into decisions</h1>
-                <p>Decongest data, identify anomalies in complex real-time analytics, and create centralized systems to enable organizations make smarter, faster, and more cost-effective decisions.</p>
+                <h1 className='tranform-data'>Transforming data into decisions</h1>
+                <p className='transform-para'>Decongest data, identify anomalies in complex real-time analytics, and create centralized systems to enable organizations make smarter, faster, and more cost-effective decisions.</p>
+                <button className='scedule-a-demo' >Schedule a demo <span className='arrow'>➝</span></button>
             </div>
+            <img className='right-side-img' src="https://framerusercontent.com/images/CkZeSiNQSOCeZlmD2gatB7yUI.png?scale-down-to=512" />
+
+        </div>
+        <div className='others-section'>
+
         </div>
       product
     </div>
